8fc9af97da2bde5b14105f2bfc401179fe77f827
greenehat <greene.aaronjames@gmail.com>
2025-02-22 15:11:40 -0700

    Character counts function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count
+from stats import char_count
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,16 +7,13 @@ def get_book_text(path_to_file):
     return file_contents
 
 
-
-
-
-
 def main():
     path_to_file = "books/frankenstein.txt"
     file_contents = get_book_text(path_to_file)
     num_words = word_count(file_contents)
+    character_count = char_count(file_contents)
     print(f"{num_words} words found in the document")
-    #print(file_contents)
+    print(character_count)
     
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def word_count(file_contents):
     count = len(file_contents.split())
     return count
+
+def char_count(file_contents):
+    chars_counted = {}
+    char_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
+    "q","r","s","t","u","v","w","x","y","z"," ",",","'",".","?",'"',"(",")"]
+    for char in char_list:
+        lowercase = file_contents.lower()
+        count = lowercase.count(char)
+        chars_counted[char] = count
+    return chars_counted
